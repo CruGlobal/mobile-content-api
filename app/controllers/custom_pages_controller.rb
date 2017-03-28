3497515e6c7d57fe3c80d92ef1b577305d89a27c
dsgoers <daniel.goers@cru.org>
2017-03-28 15:35:16 -0400

    Add endpoint for deleting a custom page

--- app/controllers/custom_pages_controller.rb
+++ app/controllers/custom_pages_controller.rb
@@ -5,6 +5,13 @@ class CustomPagesController < ApplicationController
     head(upsert_custom_page)
   end
 
+  def destroy
+    custom_page = CustomPage.find(params[:id])
+    custom_page.destroy
+  end
+
+  private
+
   def upsert_custom_page
     CustomPage.upsert(params)
   end
