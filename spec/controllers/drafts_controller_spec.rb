890c589da3207b9d544b0cbab676313e1dc5fdd0
dsgoers <daniel.goers@cru.org>
2017-03-31 15:46:59 -0400

    Rename a file

--- spec/controllers/drafts_controller_spec.rb
+++ spec/controllers/drafts_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require Rails.root.join('spec', 'support', 'test_auth_helper.rb')
+require Rails.root.join('spec', 'support', 'mock_auth_helper.rb')
 
 describe DraftsController do
   it 'does not allow unauthorized POSTs' do
