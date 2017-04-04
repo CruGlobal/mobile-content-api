35ac47955d3d7bfdd5546612e33cee2c25c34dfe
dsgoers <daniel.goers@cru.org>
2017-04-04 14:09:40 -0400

    Use custom sql to return the latest translation for each language by default

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -7,4 +7,13 @@ class ResourceSerializer < ActiveModel::Serializer
 
   has_many :translations
   has_many :pages
+
+  has_many :latest_translations
+
+  def latest_translations
+    Translation.find_by_sql("select T.* from translations T inner join (select language_id, resource_id, max(version) as
+                            max_version from translations where is_published = true and resource_id = #{object.id} group
+                            by language_id, resource_id) as max_table on T.version = max_table.max_version and
+                            T.language_id = max_table.language_id and T.resource_id = max_table.resource_id")
+  end
 end
