84cf9a46f95e875df7640db79b70d91a2797dec3
dsgoers <daniel.goers@cru.org>
2017-04-05 10:41:26 -0400

    Move big query to Resource and add a comment

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -12,4 +12,13 @@ class Resource < ActiveRecord::Base
     page_helper.push_new_onesky_translation
     Translation.create(resource: self, language: language)
   end
+
+  # Returns the latest translation for each language.  In more detailed terms, the inner query returns the highest
+  # version for the resource/language combination. The outer query then finds the translation matching that version.
+  def latest_translations
+    Translation.find_by_sql("select T.* from translations T inner join (select language_id, resource_id, max(version) as
+                            max_version from translations where is_published = true and resource_id = #{id} group
+                            by language_id, resource_id) as max_table on T.version = max_table.max_version and
+                            T.language_id = max_table.language_id and T.resource_id = max_table.resource_id")
+  end
 end
