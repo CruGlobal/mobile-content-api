63054b40fc9cfe6b2b92edad3143d06dd7fd4b34
dsgoers <daniel.goers@cru.org>
2017-04-11 10:09:01 -0400

    Move directory creation to application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'fileutils'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,8 +11,7 @@ Bundler.require(*Rails.groups)
 module MobileContentApi
   class Application < Rails::Application
     ActiveModelSerializers.config.adapter = :json_api
-
-    # probably need to put the pages directory creation here
+    FileUtils.mkdir_p('pages')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require Rails.root.join('spec', 'support', 'test_constants.rb')
-require 'fileutils'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -28,8 +27,6 @@ require 'fileutils'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
-FileUtils.mkdir_p('pages')
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
