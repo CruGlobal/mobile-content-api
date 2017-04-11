254c883dcbd44c07b4db81d871e9145a3c44e00d
dsgoers <daniel.goers@cru.org>
2017-04-11 10:03:24 -0400

    Refactor a test

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module MobileContentApi
   class Application < Rails::Application
     ActiveModelSerializers.config.adapter = :json_api
 
+    # probably need to put the pages directory creation here
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- spec/s3_helper_spec.rb
+++ spec/s3_helper_spec.rb
@@ -6,6 +6,12 @@ require 's3_helper'
 describe S3Helper do
   let(:godtools) { TestConstants::GodTools }
 
+  before(:each) do
+    mock_s3
+    mock_onesky
+    push
+  end
+
   it 'deletes temp files' do
     push
 
@@ -20,17 +26,21 @@ describe S3Helper do
 
   private
 
-  def push
+  def mock_s3
     object = double(upload_file: true)
     bucket = double(object: object)
     s3 = double(bucket: bucket)
     allow(Aws::S3::Resource).to receive(:new).and_return(s3)
+  end
 
+  def mock_onesky
     onesky_project_id = Resource.find(godtools::ID).onesky_project_id
     allow(RestClient).to receive(:get)
       .with("https://platform.api.onesky.io/1/projects/#{onesky_project_id}/translations", any_args)
       .and_return('{ "this is some json":"value" }')
+  end
 
+  def push
     translation = Translation.find(godtools::Translations::English::ID)
     s3helper = S3Helper.new(translation)
     s3helper.push_translation
