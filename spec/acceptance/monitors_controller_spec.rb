b415159b71edebcdce409fea14a3e182be4a65ab
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-04-12 11:36:10 -0400

    Fix database.yml and add monitors spec

---
+++ spec/acceptance/monitors_controller_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe MonitorsController, type: :request do
+  context '#lb' do
+    it 'gives success because we have a valid database connection' do
+      get '/monitors/lb'
+      expect(response).to be_success
+    end
+  end
+
+  context '#commit' do
+    it 'renders git GIT_COMMIT env var' do
+      ENV['GIT_COMMIT'] = 'abc123'
+
+      get '/monitors/commit'
+
+      expect(response.body).to eq 'abc123'
+    end
+  end
+end
