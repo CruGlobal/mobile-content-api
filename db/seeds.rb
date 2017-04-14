3d683038614fe9b78a2ca6640181ed145b3b3c01
dsgoers <daniel.goers@cru.org>
2017-04-13 23:47:52 -0400

    Use find_or_create_by so we can re-run seeds without deleting first

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,20 +7,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-godtools = System.create(name: 'GodTools')
-every_student = System.create(name: 'EveryStudent')
+godtools = System.find_or_create_by(name: 'GodTools')
+every_student = System.find_or_create_by(name: 'EveryStudent')
 
-kgp = Resource.create(name: 'Knowing God Personally', abbreviation: 'kgp', onesky_project_id: 148314, system: godtools)
-satisfied = Resource.create(name: 'Satisfied?', abbreviation: 'sat', onesky_project_id: 123456, system: godtools)
-Resource.create(name: 'Is There A God?', abbreviation: 'God?', onesky_project_id: 223456, system: every_student)
+kgp = Resource.find_or_create_by(name: 'Knowing God Personally', abbreviation: 'kgp', onesky_project_id: 148_314, system: godtools)
+satisfied = Resource.find_or_create_by(name: 'Satisfied?', abbreviation: 'sat', onesky_project_id: 123_456, system: godtools)
+Resource.find_or_create_by(name: 'Is There A God?', abbreviation: 'God?', onesky_project_id: 223_456, system: every_student)
 
-page_13 = Page.create(filename: '13_FinalPage.xml',
-                      resource: kgp,
-                      structure: '<note><to>Tove</to><from>Jani</from><heading>Reminder</heading>'\
+page_13 = Page.find_or_create_by(filename: '13_FinalPage.xml',
+                                 resource: kgp,
+                                 structure: '<note><to>Tove</to><from>Jani</from><heading>Reminder</heading>'\
                                  '<body>Dont forget me this weekend!</body></note>')
-Page.create(filename: '04_ThirdPoint.xml',
-                      resource: kgp,
-                      structure: '<note><to>Tove</to><from>Jani</from><heading>Reminder</heading>'\
+Page.find_or_create_by(filename: '04_ThirdPoint.xml',
+                       resource: kgp,
+                       structure: '<note><to>Tove</to><from>Jani</from><heading>Reminder</heading>'\
                                  '<body>Dont forget me this weekend!</body></note>')
 page_13_elements = [10]
 page_13_elements.push('WEBSITES TO ASSIST YOU')
@@ -34,30 +35,30 @@ page_13_elements.push('jesusfilm.org/watch/jesus.html/english.html')
 page_13_elements.push('Email the above links')
 
 page_13_elements.each do |e|
-  TranslationElement.create(page: page_13, text: e)
+  TranslationElement.find_or_create_by(page: page_13, text: e)
 end
 
-english = Language.create(name: 'English', abbreviation: 'en')
-german = Language.create(name: 'German', abbreviation: 'de')
-Language.create(name: 'Slovak', abbreviation: 'sk')
+english = Language.find_or_create_by(name: 'English', abbreviation: 'en')
+german = Language.find_or_create_by(name: 'German', abbreviation: 'de')
+Language.find_or_create_by(name: 'Slovak', abbreviation: 'sk')
 
-Translation.create(resource: kgp, language: english, version: 1, is_published: true)
-Translation.create(resource: kgp, language: german, version: 1, is_published: true)
-german_kgp = Translation.create(resource: kgp, language: german, version: 2)
+Translation.find_or_create_by(resource: kgp, language: english, version: 1, is_published: true)
+Translation.find_or_create_by(resource: kgp, language: german, version: 1, is_published: true)
+german_kgp = Translation.find_or_create_by(resource: kgp, language: german, version: 2)
 
-CustomPage.create(translation: german_kgp,
-                  page: page_13,
-                  structure: '<custom>This is some custom xml for one translation</custom>')
+CustomPage.find_or_create_by(translation: german_kgp,
+                             page: page_13,
+                             structure: '<custom>This is some custom xml for one translation</custom>')
 
-Translation.create(resource: satisfied, language: english, version: 1, is_published: true)
-Translation.create(resource: satisfied, language: english, version: 2, is_published: true)
-Translation.create(resource: satisfied, language: english, version: 3)
-Translation.create(resource: satisfied, language: german, version: 1, is_published: true)
-Translation.create(resource: satisfied, language: german, version: 2, is_published: true)
+Translation.find_or_create_by(resource: satisfied, language: english, version: 1, is_published: true)
+Translation.find_or_create_by(resource: satisfied, language: english, version: 2, is_published: true)
+Translation.find_or_create_by(resource: satisfied, language: english, version: 3)
+Translation.find_or_create_by(resource: satisfied, language: german, version: 1, is_published: true)
+Translation.find_or_create_by(resource: satisfied, language: german, version: 2, is_published: true)
 
-AccessCode.create(code: 123456)
+AccessCode.find_or_create_by(code: 123_456)
 
-Attribute.create(resource: kgp, key: 'Banner Image', value: 'this is a location')
-attribute = Attribute.create(resource: kgp, key: 'translate me', value: 'base language', is_translatable: true)
+Attribute.find_or_create_by(resource: kgp, key: 'Banner Image', value: 'this is a location')
+attribute = Attribute.find_or_create_by(resource: kgp, key: 'translate me', value: 'base language', is_translatable: true)
 
-TranslatedAttribute.create(parent_attribute: attribute, translation: german_kgp, value: 'german attribute')
+TranslatedAttribute.find_or_create_by(parent_attribute: attribute, translation: german_kgp, value: 'german attribute')
