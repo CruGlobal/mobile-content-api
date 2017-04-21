89a86ba2a61c9fd3a3f8d5f9517b14a80b938b06
Ryan Carlson <ryan.t.carlson@cru.org>
2017-04-21 16:09:00 -0400

    Add index endpoint to translations controller
    To allow clients to download list of translations, filtered by is_published for now, and include other things that it cares about.

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 class TranslationsController < ApplicationController
+  
+  def index
+    render json: all_translations, include: params[:include], status: :ok
+  end
+  
   def show
     redirect
   end
 
   private
 
+  def all_translations
+    if params['filter']
+      Translation.is_published(params['filter']['is_published'])
+    else
+      Translation.all
+    end
+  end
+      
   def redirect
     translation = Translation.find(params[:id])
     redirect_to translation.s3_uri, status: :found
