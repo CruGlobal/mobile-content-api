77d75b43d3b7cc8d3c4fdc67929c9a2c690ee235
dsgoers <daniel.goers@cru.org>
2017-04-21 13:30:18 -0400

    Handle json api content type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :decode_json_api
+
   def render(**args)
     if args.key? :json
       response.headers['Content-Type'] = 'application/vnd.api+json'
     end
 
     super
+  end
+
+  private
+
+  def decode_json_api
+    return if request.headers['REQUEST_METHOD'] == 'GET' ||
+              request.headers['Content-Type'] != 'application/vnd.api+json'
+    params.merge!(ActiveSupport::JSON.decode(request.body.string))
   end
 end
