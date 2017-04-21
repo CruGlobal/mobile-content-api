681548a657e7579f4489e02651b79a71e8c7b925
dsgoers <daniel.goers@cru.org>
2017-04-21 14:57:06 -0400

    Ignore routing errors in Rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -29,7 +29,7 @@ Rollbar.configure do |config|
   # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
   #
   # You can also specify a callable, which will be called with the exception instance.
-  # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
