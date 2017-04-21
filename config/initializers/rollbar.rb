b8726e2438ba3231e9d73a68206d1d09f94ec93d
dsgoers <daniel.goers@cru.org>
2017-04-21 15:01:39 -0400

    Rubocop

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -29,7 +29,7 @@ Rollbar.configure do |config|
   # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
   #
   # You can also specify a callable, which will be called with the exception instance.
-  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
+  config.exception_level_filters['ActionController::RoutingError'] = 'ignore'
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
