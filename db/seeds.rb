0693b16bcfbb1506af8b59a445e371a84f2cb917
dsgoers <daniel.goers@cru.org>
2017-04-27 13:56:58 -0400

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,11 +32,24 @@ page_13 = Page.find_or_create_by(filename: '13_FinalPage.xml',
     </base_xml_element>
   </hero>
 </page>')
-Page.find_or_create_by(filename: '04_ThirdPoint.xml',
+page_4 = Page.find_or_create_by(filename: '04_ThirdPoint.xml',
                        resource: kgp,
-                       structure: '<note><to>Tove</to><from>Jani</from><heading>Reminder</heading>'\
-                                 '<body>Dont forget me this weekend!</body></note>')
-page_13_elements = [10]
+                       structure: '<?xml version="1.0" encoding="UTF-8" ?>
+<page xmlns="http://mobile-content-api.cru.org/xmlns/tract"
+      xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
+  <hero>
+    <heading>
+      <content:text i18n-id="14">fourteen un-translated phrase</content:text>
+      <content:text i18n-id="15">fifteen un-translated phrase</content:text>
+    </heading>
+
+    <base_xml_element>
+      <content:text i18n-id="{{uuid}}">We are sinful and separated from God because of our sin.
+      </content:text>
+    </base_xml_element>
+  </hero>
+</page>')
+page_13_elements = []
 page_13_elements.push('WEBSITES TO ASSIST YOU')
 page_13_elements.push('Still not sure who Jesus is?')
 page_13_elements.push('everystudent.com')
@@ -50,6 +63,31 @@ page_13_elements.push('Email the above links')
 
 page_13_elements.each do |e|
   TranslationElement.find_or_create_by(page: page_13, text: e)
+end
+
+page_4_elements = []
+page_4_elements.push('JESUS IS GOD\'S ONLY SOLUTION FOR OUR SIN. ONLY THROUGH HIM CAN WE KNOW GOD AND RECEIVE HIS LOVE AND FORGIVENESS.')
+page_4_elements.push('Jesus is God')
+page_4_elements.push('Christ is the visible image of the invisible God. He existed before anything was created and is supreme over all creation.')
+page_4_elements.push('- Colossians 1:15')
+page_4_elements.push('Jesus Died In Our Place')
+page_4_elements.push('Christ suffered for our sins once for all time. He never sinned, but he died for sinners to bring you home safely to God.')
+page_4_elements.push('- 1 Peter 3:18a')
+page_4_elements.push('Jesus Came Back to Life')
+page_4_elements.push('During the forty days after his crucifixion, he appeared to the apostles ... and he proved to them in many ways he was actually alive.   - Acts 1:3a')
+page_4_elements.push('His resurrection proved that he was God and that he had suffered the punishment we deserved in our place.')
+page_4_elements.push('Jesus is the Only Way...')
+page_4_elements.push('Jesus said, "I am the way, the truth and the life. No-one can come to the Father except through me."')
+page_4_elements.push('- John 14:6')
+page_4_elements.push('God Proves His Love...')
+page_4_elements.push('For God loved the world so much that he gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. - John 3:16')
+page_4_elements.push('▲ - God  |  † - Jesus  |  ★ - Man')
+page_4_elements.push('Although we deserve to be cut off from God forever, in his love God sent Jesus to pay the penalty for our sins by dying on the cross.')
+page_4_elements.push('Through Jesus, God has bridged the gap that separates us from him, and provided a way for us to be forgiven and restored to relationship with him.')
+page_4_elements.push('It\'s not enough just to know these points...')
+
+page_4_elements.each do |e|
+  TranslationElement.find_or_create_by(page: page_4, text: e)
 end
 
 english = Language.find_or_create_by(name: 'English', code: 'en')
