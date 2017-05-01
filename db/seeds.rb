e08955dba7e197996e937f3928bd6d037796ae27
dsgoers <daniel.goers@cru.org>
2017-05-01 15:51:26 -0400

    Shuffle seeds file around to look nicer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-godtools = System.find_or_create_by(name: 'GodTools')
-every_student = System.find_or_create_by(name: 'EveryStudent')
-
-kgp = Resource.find_or_create_by(name: 'Knowing God Personally', abbreviation: 'kgp', onesky_project_id: 148_314, system: godtools)
-satisfied = Resource.find_or_create_by(name: 'Satisfied?', abbreviation: 'sat', onesky_project_id: 123_456, system: godtools)
-is_there_god = Resource.find_or_create_by(name: 'Is There A God?', abbreviation: 'God?', onesky_project_id: 223_456, system: every_student)
-
-page_13 = Page.find_or_create_by(filename: '13_FinalPage.xml',
-                                 resource: kgp,
-                                 structure: '<?xml version="1.0" encoding="UTF-8" ?>
+page_13_structure = '<?xml version="1.0" encoding="UTF-8" ?>
 <page xmlns="http://mobile-content-api.cru.org/xmlns/tract"
       xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
   <hero>
@@ -31,76 +22,9 @@ page_13 = Page.find_or_create_by(filename: '13_FinalPage.xml',
       </content:text>
     </base_xml_element>
   </hero>
-</page>')
-page_4 = Page.find_or_create_by(filename: '04_ThirdPoint.xml',
-                       resource: kgp,
-                       structure: '<?xml version="1.0" encoding="UTF-8" ?>
-<page xmlns="http://mobile-content-api.cru.org/xmlns/tract"
-      xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
-  <hero>
-    <heading>
-      <content:text i18n-id="1373aa60-6c3f-4c69-b5ad-acfa2c0e4540">one un-translated phrase</content:text>
-      <content:text i18n-id="e68a67da-df02-493f-b138-661bfe120663">two un-translated phrase</content:text>
-    </heading>
+</page>'
 
-    <base_xml_element>
-      <content:text>We are sinful and separated from God because of our sin.
-      </content:text>
-    </base_xml_element>
-  </hero>
-</page>')
-TranslationElement.find_or_create_by(page: page_13, text: 'WEBSITES TO ASSIST YOU', onesky_phrase_id: 'f9894df9-df1d-4831-9782-345028c6c9a2')
-TranslationElement.find_or_create_by(page: page_13, text: 'Still not sure who Jesus is?', onesky_phrase_id: '9deda19f-c3ee-42ed-a1eb-92423e543352')
-page_13_elements = []
-page_13_elements.push('everystudent.com')
-page_13_elements.push('Read the Bible')
-page_13_elements.push('bible.com/bible/59/mrk.1')
-page_13_elements.push('More about Christianity...')
-page_13_elements.push('startingwithgod.com')
-page_13_elements.push('Watch a film about Jesus')
-page_13_elements.push('jesusfilm.org/watch/jesus.html/english.html')
-page_13_elements.push('Email the above links')
-
-page_13_elements.each do |e|
-  TranslationElement.find_or_create_by(page: page_13, text: e)
-end
-
-page_4_elements = []
-page_4_elements.push('JESUS IS GOD\'S ONLY SOLUTION FOR OUR SIN. ONLY THROUGH HIM CAN WE KNOW GOD AND RECEIVE HIS LOVE AND FORGIVENESS.')
-page_4_elements.push('Jesus is God')
-page_4_elements.push('Christ is the visible image of the invisible God. He existed before anything was created and is supreme over all creation.')
-page_4_elements.push('- Colossians 1:15')
-page_4_elements.push('Jesus Died In Our Place')
-page_4_elements.push('Christ suffered for our sins once for all time. He never sinned, but he died for sinners to bring you home safely to God.')
-page_4_elements.push('- 1 Peter 3:18a')
-page_4_elements.push('Jesus Came Back to Life')
-page_4_elements.push('During the forty days after his crucifixion, he appeared to the apostles ... and he proved to them in many ways he was actually alive.   - Acts 1:3a')
-page_4_elements.push('His resurrection proved that he was God and that he had suffered the punishment we deserved in our place.')
-page_4_elements.push('Jesus is the Only Way...')
-page_4_elements.push('Jesus said, "I am the way, the truth and the life. No-one can come to the Father except through me."')
-page_4_elements.push('- John 14:6')
-page_4_elements.push('God Proves His Love...')
-page_4_elements.push('For God loved the world so much that he gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. - John 3:16')
-page_4_elements.push('▲ - God  |  † - Jesus  |  ★ - Man')
-page_4_elements.push('Although we deserve to be cut off from God forever, in his love God sent Jesus to pay the penalty for our sins by dying on the cross.')
-page_4_elements.push('Through Jesus, God has bridged the gap that separates us from him, and provided a way for us to be forgiven and restored to relationship with him.')
-page_4_elements.push('It\'s not enough just to know these points...')
-
-page_4_elements.each do |e|
-  TranslationElement.find_or_create_by(page: page_4, text: e)
-end
-
-english = Language.find_or_create_by(name: 'English', code: 'en')
-german = Language.find_or_create_by(name: 'German', code: 'de')
-Language.find_or_create_by(name: 'Slovak', code: 'sk')
-
-Translation.find_or_create_by(resource: kgp, language: english, version: 1, is_published: true)
-Translation.find_or_create_by(resource: kgp, language: german, version: 1, is_published: true)
-german_kgp = Translation.find_or_create_by(resource: kgp, language: german, version: 2)
-
-CustomPage.find_or_create_by(translation: german_kgp,
-                             page: page_13,
-                             structure: '<?xml version="1.0" encoding="UTF-8" ?>
+page_13_custom_structure = '<?xml version="1.0" encoding="UTF-8" ?>
 <page xmlns="http://mobile-content-api.cru.org/xmlns/tract"
       xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
   <hero>
@@ -115,7 +39,69 @@ CustomPage.find_or_create_by(translation: german_kgp,
       </content:text>
     </custom_xml_element>
   </hero>
-</page>')
+</page>'
+
+page_13_elements = ['everystudent.com', 'Read the Bible', 'bible.com/bible/59/mrk.1', 'More about Christianity...',
+                    'startingwithgod.com', 'Watch a film about Jesus', 'jesusfilm.org/watch/jesus.html/english.html',
+                    'Email the above links']
+
+page_4_structure = '<?xml version="1.0" encoding="UTF-8" ?>
+<page xmlns="http://mobile-content-api.cru.org/xmlns/tract"
+      xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
+  <hero>
+    <heading>
+      <content:text i18n-id="1373aa60-6c3f-4c69-b5ad-acfa2c0e4540">one un-translated phrase</content:text>
+      <content:text i18n-id="e68a67da-df02-493f-b138-661bfe120663">two un-translated phrase</content:text>
+    </heading>
+
+    <base_xml_element>
+      <content:text>We are sinful and separated from God because of our sin.
+      </content:text>
+    </base_xml_element>
+  </hero>
+</page>'
+
+page_4_elements = ['JESUS IS GOD\'S ONLY SOLUTION FOR OUR SIN. ONLY THROUGH HIM CAN WE KNOW GOD AND RECEIVE HIS LOVE AND FORGIVENESS.',
+                   'Jesus is God', 'Christ is the visible image of the invisible God. He existed before anything was created and is supreme over all creation.',
+                   '- Colossians 1:15', 'Jesus Died In Our Place', 'Christ suffered for our sins once for all time. He never sinned, but he died for sinners to bring you home safely to God.',
+                   '- 1 Peter 3:18a', 'Jesus Came Back to Life', 'During the forty days after his crucifixion, he appeared to the apostles ... and he proved to them in many ways he was actually alive.   - Acts 1:3a',
+                   'His resurrection proved that he was God and that he had suffered the punishment we deserved in our place.', 'Jesus is the Only Way...',
+                   'Jesus said, "I am the way, the truth and the life. No-one can come to the Father except through me."', '- John 14:6',
+                   'God Proves His Love...', 'For God loved the world so much that he gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. - John 3:16',
+                   '▲ - God  |  † - Jesus  |  ★ - Man', 'Although we deserve to be cut off from God forever, in his love God sent Jesus to pay the penalty for our sins by dying on the cross.',
+                   'Through Jesus, God has bridged the gap that separates us from him, and provided a way for us to be forgiven and restored to relationship with him.', 'It\'s not enough just to know these points...']
+
+
+godtools = System.find_or_create_by(name: 'GodTools')
+every_student = System.find_or_create_by(name: 'EveryStudent')
+
+kgp = Resource.find_or_create_by(name: 'Knowing God Personally', abbreviation: 'kgp', onesky_project_id: 148_314, system: godtools)
+satisfied = Resource.find_or_create_by(name: 'Satisfied?', abbreviation: 'sat', onesky_project_id: 123_456, system: godtools)
+is_there_god = Resource.find_or_create_by(name: 'Is There A God?', abbreviation: 'God?', onesky_project_id: 223_456, system: every_student)
+
+page_13 = Page.find_or_create_by(filename: '13_FinalPage.xml', resource: kgp, structure: page_13_structure)
+page_4 = Page.find_or_create_by(filename: '04_ThirdPoint.xml', resource: kgp, structure: page_4_structure)
+
+TranslationElement.find_or_create_by(page: page_13, text: 'WEBSITES TO ASSIST YOU', onesky_phrase_id: 'f9894df9-df1d-4831-9782-345028c6c9a2')
+TranslationElement.find_or_create_by(page: page_13, text: 'Still not sure who Jesus is?', onesky_phrase_id: '9deda19f-c3ee-42ed-a1eb-92423e543352')
+
+page_13_elements.each do |e|
+  TranslationElement.find_or_create_by(page: page_13, text: e)
+end
+
+page_4_elements.each do |e|
+  TranslationElement.find_or_create_by(page: page_4, text: e)
+end
+
+english = Language.find_or_create_by(name: 'English', code: 'en')
+german = Language.find_or_create_by(name: 'German', code: 'de')
+Language.find_or_create_by(name: 'Slovak', code: 'sk')
+
+Translation.find_or_create_by(resource: kgp, language: english, version: 1, is_published: true)
+Translation.find_or_create_by(resource: kgp, language: german, version: 1, is_published: true)
+german_kgp = Translation.find_or_create_by(resource: kgp, language: german, version: 2)
+
+CustomPage.find_or_create_by(translation: german_kgp, page: page_13, structure: page_13_custom_structure)
 
 Translation.find_or_create_by(resource: satisfied, language: english, version: 1, is_published: true)
 Translation.find_or_create_by(resource: satisfied, language: english, version: 2, is_published: true)
