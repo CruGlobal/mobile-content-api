94cff7bfd83d57eb928d8036222da0a655f5b017
dsgoers <daniel.goers@cru.org>
2017-05-08 14:20:23 -0400

    Remove protect_from_forgery since this is an API and we do our own authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :decode_json_api
 
   def render(**args)
