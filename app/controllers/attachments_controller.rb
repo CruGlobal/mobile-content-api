fd9c77d2fb305a0b3c142887940a63ec8509848e
dsgoers <daniel.goers@cru.org>
2017-05-09 11:11:10 -0400

    Add update and destroy methods for attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,27 @@
 
 class AttachmentsController < SecureController
   def create
-    Attachment.create!(params.permit(:key, :resource_id, :translation_id, :file))
+    Attachment.create!(params.permit(permitted_params))
     head :created
+  end
+
+  def update
+    load_attachment.update(params.permit(permitted_params))
+    head :no_content
+  end
+
+  def destroy
+    load_attachment.destroy
+    head :no_content
+  end
+
+  private
+
+  def load_attachment
+    Attachment.find(params[:id])
+  end
+
+  def permitted_params
+    [:key, :resource_id, :translation_id, :file]
   end
 end
--- spec/acceptance/attachments_controller_spec.rb
+++ spec/acceptance/attachments_controller_spec.rb
@@ -4,6 +4,7 @@ require 'acceptance_helper'
 
 resource 'Attachments' do
   let(:test_file) { Rack::Test::UploadedFile.new('public/wall.jpg', 'image/png') }
+  let(:authorization) { AuthToken.create(access_code: AccessCode.find(1)).token }
 
   post 'attachments/' do
     it 'does not allow unauthorized requests', document: false do
@@ -15,11 +16,37 @@ resource 'Attachments' do
     end
 
     it 'create an Attachment' do
-      header 'Authorization', AuthToken.create(access_code: AccessCode.find(1)).token
+      header 'Authorization', :authorization
 
       do_request file: test_file, key: 'test_image', multipart: true, resource_id: 1
 
       expect(status).to be(201)
+    end
+  end
+
+  put 'attachments/:id' do
+    header 'Authorization', :authorization
+    let(:id) { 1 }
+
+    it 'update an Attachment' do
+      header 'Authorization', AuthToken.create(access_code: AccessCode.find(1)).token
+
+      do_request file: test_file, key: 'test_image', multipart: true, resource_id: 1
+
+      expect(status).to be(204)
+    end
+  end
+
+  delete 'attachments/:id' do
+    header 'Authorization', :authorization
+    let(:id) { 1 }
+
+    it 'delete an Attachment' do
+      header 'Authorization', AuthToken.create(access_code: AccessCode.find(1)).token
+
+      do_request
+
+      expect(status).to be(204)
     end
   end
 end
