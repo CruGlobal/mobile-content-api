96ed04e87eb179c35db29e9437ae682a9d9a05f9
dsgoers <daniel.goers@cru.org>
2017-05-09 10:38:52 -0400

    Improve a couple tests

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -37,15 +37,15 @@ describe Attachment do
       .to(raise_error('Key is currently used by an Attribute.'))
   end
 
-  it 'may not duplicate key and resource id' do
-    result = Attachment.create(key: 'banner_image', file: test_file, resource_id: 2)
+  it 'may not duplicate key and resource id and is not case sensitive' do
+    result = Attachment.create(key: 'banner_IMAge', file: test_file, resource_id: 2)
 
     expect(result).to_not be_valid
     expect(result.errors[:resource]).to include 'has already been taken'
   end
 
   it 'may not duplicate key and translation id' do
-    result = Attachment.create(key: 'german_kgp_logo', file: test_file, translation_id: 3)
+    result = Attachment.create(key: 'german_KGP_logo', file: test_file, translation_id: 3)
 
     expect(result).to_not be_valid
     expect(result.errors[:translation]).to include 'has already been taken'
