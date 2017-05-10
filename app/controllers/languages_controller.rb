a2a9cb42559137bc93e2cf4434b6ae8b55b20f66
dsgoers <daniel.goers@cru.org>
2017-05-10 15:51:00 -0400

    Add create and delete methods for languages/

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 
 class LanguagesController < ApplicationController
+  before_action :authorize!, only: [:create, :destroy]
+
   def index
-    all_languages
+    render json: Language.all, status: :ok
   end
 
   def show
-    language
+    render json: load_language, status: :ok
+  end
+
+  def create
+    language = Language.create!(params.require(:data).require(:attributes).permit([:name, :code]))
+    head :created, location: "languages/#{language.id}"
+  end
+
+  def destroy
+    load_language.destroy!
+    head :no_content
   end
 
   private
 
-  def all_languages
-    render json: Language.all, status: :ok
-  end
-
-  def language
-    render json: Language.find(params[:id]), status: :ok
+  def load_language
+    Language.find(params[:id])
   end
 end
--- spec/acceptance/languages_controller_spec.rb
+++ spec/acceptance/languages_controller_spec.rb
@@ -7,6 +7,7 @@ resource 'Languages' do
   header 'Content-Type', 'application/vnd.api+json'
 
   let(:raw_post) { params.to_json }
+  let(:authorization) { AuthToken.create(access_code: AccessCode.find(1)).token }
 
   get 'languages' do
     it 'get all languages' do
@@ -23,6 +24,45 @@ resource 'Languages' do
       do_request
 
       expect(status).to be(200)
+    end
+  end
+
+  post 'languages' do
+    it 'requires authorization', document: false do
+      header 'Authorization', nil
+
+      do_request data: { type: :language, attributes: { name: 'Elvish', code: 'ev' } }
+
+      expect(status).to be(401)
+    end
+
+    it 'create a language' do
+      header 'Authorization', authorization
+
+      do_request data: { type: :language, attributes: { name: 'Elvish', code: 'ev' } }
+
+      expect(status).to be(201)
+      expect(response_headers['Location']).to match(%r{languages\/\d+})
+    end
+  end
+
+  delete 'languages/:id' do
+    let(:id) { 1 }
+
+    it 'requires authorization', document: false do
+      header 'Authorization', nil
+
+      do_request
+
+      expect(status).to be(401)
+    end
+
+    it 'delete a language' do
+      header 'Authorization', authorization
+
+      do_request
+
+      expect(status).to be(204)
     end
   end
 end
