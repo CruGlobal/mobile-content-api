6b3331a199dd9a91d81783c41721ec99671362c5
dsgoers <daniel.goers@cru.org>
2017-05-11 09:20:38 -0400

    Improve error message

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -10,8 +10,8 @@ class TranslationsController < ApplicationController
     if @translation.is_published
       redirect
     else
-      @translation.errors.add(:message,
-                              'Translation not found.  Use drafts/ if you\'re looking for an unpublished translation.')
+      @translation.errors.add(:message, 'Translation not found. '\
+                                        "Use drafts/{id} if you're looking for an unpublished translation.")
       render_error(@translation, :not_found)
     end
   end
