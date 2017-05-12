83ce22e4236a7fc71052d23654fddd0a10da35d6
dsgoers <daniel.goers@cru.org>
2017-05-12 11:05:58 -0400

    Remove relationship between Attachments and Translations as we may add that sometime later

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -9,29 +9,6 @@ describe Attachment do
     allow_any_instance_of(Paperclip::Attachment).to receive(:save).and_return(true)
   end
 
-  it 'may not have nil for Resource and Translation' do
-    expect { Attachment.create(key: 'test', file: test_file, resource_id: nil, translation_id: nil) }.to(
-      raise_error('Attachment must be related to Resource or Translation.')
-    )
-  end
-
-  it 'may have nil for Resource and value for Translation' do
-    result = Attachment.create(key: 'test', file: test_file, resource_id: nil, translation_id: 1)
-
-    expect(result).to be_valid
-  end
-
-  it 'may have value for Resource and nil for Translation' do
-    result = Attachment.create(key: 'test', file: test_file, resource_id: 1, translation_id: nil)
-
-    expect(result).to be_valid
-  end
-
-  it 'may not have value for Resource and value for Translation' do
-    expect { Attachment.create(key: 'test', file: test_file, resource_id: 1, translation_id: 1) }
-      .to(raise_error('Attachment can be related to Resource OR Translation, not both.'))
-  end
-
   it 'may not have the same key and resource id as an Attribute' do
     expect { Attachment.create!(key: 'banner_image', file: test_file, resource_id: 1) }
       .to(raise_error('Key is currently used by an Attribute.'))
@@ -42,12 +19,5 @@ describe Attachment do
 
     expect(result).to_not be_valid
     expect(result.errors[:resource]).to include 'has already been taken'
-  end
-
-  it 'may not duplicate key and translation id' do
-    result = Attachment.create(key: 'german_KGP_logo', file: test_file, translation_id: 3)
-
-    expect(result).to_not be_valid
-    expect(result.errors[:translation]).to include 'has already been taken'
   end
 end
