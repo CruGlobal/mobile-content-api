502a7c2db180180bf7ab0310d4ea46fddce01533
dsgoers <daniel.goers@cru.org>
2017-05-17 16:12:50 -0400

    Don't document authorization tests

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -9,12 +9,12 @@ RspecApiDocumentation.configure do |config|
 end
 
 def requires_authorization
-  it 'requires authorization' do
+  it 'requires authorization', document: false do
     header 'Authorization', nil
 
     do_request
 
     expect(status).to be(401)
-    expect(JSON.parse(response_body)['data']).to be(nil)
+    expect(JSON.parse(response_body)['data']).to be_nil
   end
 end
