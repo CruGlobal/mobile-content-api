cf4867b717a085600cbb53c32ad61461a7db41d1
dsgoers <daniel.goers@cru.org>
2017-06-06 11:52:48 -0400

    Don't allow views quantity to be less than 1

--- app/models/view.rb
+++ app/models/view.rb
@@ -3,6 +3,6 @@
 class View < ActiveRecord::Base
   belongs_to :resource
 
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :resource, presence: true
 end
---
+++ spec/models/view_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe View do
+  it 'must be greater than zero' do
+    result = described_class.create(resource_id: 1, quantity: 0)
+
+    expect(result).not_to be_valid
+    expect(result.errors[:quantity]).to include('must be greater than 0')
+  end
+end
