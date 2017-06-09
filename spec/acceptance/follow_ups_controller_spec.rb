141abda7a07557f3ab60c63c2ae755c86e051121
dsgoers <daniel.goers@cru.org>
2017-06-09 10:37:02 -0400

    Use seed data for simpler tests

--- spec/acceptance/follow_ups_controller_spec.rb
+++ spec/acceptance/follow_ups_controller_spec.rb
@@ -6,16 +6,11 @@ resource 'Follow Ups' do
   header 'Accept', 'application/vnd.api+json'
   header 'Content-Type', 'application/vnd.api+json'
   let(:raw_post) { params.to_json }
-  let(:destination_id) { 123 }
-
-  before do
-    allow(Destination).to receive(:find).with(destination_id).and_return(Destination.new(id: destination_id))
-  end
 
   post 'follow_ups/' do
     let(:data) do
       { type: :follow_up,
-        attributes: { name: 'Billy Bob', email: 'bob@test.com', language_id: 2, destination_id: destination_id } }
+        attributes: { name: 'Billy Bob', email: 'bob@test.com', language_id: 2, destination_id: 1 } }
     end
 
     it 'create a subscriber' do
--- spec/models/follow_up_spec.rb
+++ spec/models/follow_up_spec.rb
@@ -5,23 +5,13 @@ require 'rails_helper'
 require 'validates_email_format_of/rspec_matcher'
 
 describe FollowUp do
-  let(:destination) do
-    Destination.new(id: 123,
-                    route_id: '12',
-                    url: 'myapi.org',
-                    access_key_id: '123456',
-                    access_key_secret: 'hello, world!')
-  end
+  let(:destination) { Destination.find(1) }
   let(:email) { 'bob@test.org' }
   let(:language) { Language.find(2) }
   let(:language_id) { 3 }
   let(:first_name) { 'Bob' }
   let(:last_name) { 'Test' }
   let(:full_name) { "#{first_name} #{last_name}" }
-
-  before do
-    allow(Destination).to receive(:find).with(123).and_return(destination)
-  end
 
   it 'validates email address' do
     result = described_class.new('myemail', language.id, destination.id, full_name)
