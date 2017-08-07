7e396c25b7286db91cf1f55ed9a081704751e834
dsgoers <daniel.goers@cru.org>
2017-08-07 10:47:46 -0400

    Add test for expired token on secure methods of non-secure controllers

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -9,12 +9,37 @@ RspecApiDocumentation.configure do |config|
 end
 
 def requires_authorization
-  it 'requires authorization', document: false do
-    header 'Authorization', nil
-
-    do_request
-
-    expect(status).to be(401)
-    expect(JSON.parse(response_body)['data']).to be_nil
+  it 'must send a token', document: false do
+    blank
   end
+
+  it 'cannot use an expired token', document: false do
+    expired
+  end
+end
+
+private
+
+def blank
+  header 'Authorization', nil
+
+  do_request
+
+  expect(status).to be(401)
+  expect(JSON.parse(response_body)['data']).to be_nil
+end
+
+def expired
+  header 'Authorization', expired_token
+
+  do_request
+
+  expect(status).to be(401)
+  expect(JSON.parse(response_body)['data']).to be_nil
+end
+
+def expired_token
+  auth = AuthToken.create!(access_code: AccessCode.find(1))
+  auth.update!(expiration: DateTime.now.utc - 25.hours)
+  auth.token
 end
