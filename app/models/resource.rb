60cc0dc1de98d30c971ec5203a3cdb743828fd1c
dsgoers <daniel.goers@cru.org>
2017-08-18 16:36:27 -0400

    More refactoring

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -35,7 +35,7 @@ class Resource < ActiveRecord::Base
   end
 
   def latest_translations
-    latest('is_published = true and')
+    latest(true)
   end
 
   def latest_drafts_translations
@@ -51,7 +51,7 @@ class Resource < ActiveRecord::Base
   private
 
   # returns the Translation with the highest version for each Language and this Resource
-  def latest(is_published = nil)
+  def latest(is_published = [true, false])
     Translation.joins("inner join (#{latest_versions(is_published).to_sql}) as max_table
                        on translations.version = max_table.max_version
                        and translations.language_id = max_table.language_id
@@ -61,8 +61,7 @@ class Resource < ActiveRecord::Base
   # returns the highest version for each Language and this Resource
   def latest_versions(is_published)
     Translation.select(:language_id, :resource_id, 'max(version) as max_version')
-               .from('translations')
-               .where("#{is_published} resource_id = #{id}")
+               .where(resource_id: id, is_published: is_published)
                .group(:language_id, :resource_id)
   end
 end
