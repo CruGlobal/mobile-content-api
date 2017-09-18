07ec95ec8ac529be2554492f8dfff2959afcb4c9
dsgoers <daniel.goers@cru.org>
2017-09-18 10:03:03 -0400

    Refactor some conditional code

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,7 +6,7 @@ class Attachment < ActiveRecord::Base
   validates :file, presence: true
   validates :is_zipped, inclusion: { in: [true, false] }
   validates :resource, presence: true, uniqueness: { scope: :file_file_name }
-  validates_with AttachmentValidator
+  validates_with AttachmentValidator, if: :queued?
 
   belongs_to :resource
 
@@ -14,7 +14,11 @@ class Attachment < ActiveRecord::Base
   validates_attachment :file, content_type: { content_type: %w(image/jpg image/jpeg image/png image/gif) }
 
   before_validation :set_defaults
-  before_save :save_sha256
+  before_save :save_sha256, if: :queued?
+
+  def queued?
+    file.queued_for_write[:original]
+  end
 
   private
 
@@ -23,9 +27,6 @@ class Attachment < ActiveRecord::Base
   end
 
   def save_sha256
-    queued = file.queued_for_write[:original]
-    return unless queued
-
-    self.sha256 = XmlUtil.filename_sha(open(queued.path).read)
+    self.sha256 = XmlUtil.filename_sha(open(file.queued_for_write[:original].path).read)
   end
 end
--- app/validators/attachment_validator.rb
+++ app/validators/attachment_validator.rb
@@ -2,10 +2,8 @@
 
 class AttachmentValidator < ActiveModel::Validator
   def validate(a)
-    queued = a.file.queued_for_write[:original]
-    return unless queued
 
-    return unless Attachment.exists?(sha256: XmlUtil.filename_sha(open(queued.path).read), resource: a.resource)
+    return unless Attachment.exists?(sha256: XmlUtil.filename_sha(open(a.file.queued_for_write[:original].path).read), resource: a.resource)
     a.errors.add(:file, 'This file already exists for this resource')
   end
 end
