14f383fe80eaaa3d6f439a530628d1835f764209
ryancarlson <ryan.t.carlson@cru.org>
2018-04-16 17:13:50 -0400

    Remove default empty string from translations.translated_tagline  - and migrate any empty strings to nil

---
+++ db/migrate/20180416210608_translations_tagline_drop_default.rb
@@ -0,0 +1,6 @@
+class TranslationsTaglineDropDefault < ActiveRecord::Migration[5.0]
+  def change
+    change_column_default(:translations, :translated_tagline, nil)
+    Translation.where( translated_tagline: '' ).update_all( translated_tagline: nil )
+  end
+end
