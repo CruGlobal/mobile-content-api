0d618957a6d24830628b94f3706a9eb00be32762
Justin Sabelko <justin.sabelko@cru.org>
2019-09-24 15:02:14 -0500

    Handle both URL and local file

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -32,6 +32,8 @@ class Attachment < ActiveRecord::Base
 
   def generate_sha256
     XmlUtil.filename_sha(URI.parse(url).open.read)
+  rescue NoMethodError
+    XmlUtil.filename_sha(File.open(url).read)
   end
 
   private
