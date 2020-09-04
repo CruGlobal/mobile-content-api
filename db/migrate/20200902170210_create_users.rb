d4d1885a147a0a2d5d3a285da3afbd21b41c3f60
Spencer Oberstadt <soberstadt@gmail.com>
2020-09-04 10:59:48 -0400

    don't fail migration if we can't enable citext

--- db/migrate/20200902170210_create_users.rb
+++ db/migrate/20200902170210_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    enable_extension "citext"
+    begin
+      enable_extension "citext"
+    rescue ActiveRecord::StatementInvalid
+    end
 
     create_table :users do |t|
       t.string :first_name
