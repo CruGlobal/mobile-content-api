06fb796141340019a17a5bc7bd54218a84644303
Spencer Oberstadt <soberstadt@gmail.com>
2020-09-04 12:11:47 -0400

    add failing spec

--- spec/factories/tips.rb
+++ spec/factories/tips.rb
@@ -1,7 +1,25 @@
 FactoryBot.define do
   factory :tip do
+    structure do
+      <<~XML
+      <tip xmlns="https://mobile-content-api.cru.org/xmlns/training"
+        xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
+          <pages>
+              <page>
+                  <content:paragraph>
+                      <content:text />
+                  </content:paragraph>
+                  <content:text />
+              </page>
+          </pages>
+      </tip>
+      XML
+    end
     sequence :name do |n|
       "name_#{n}"
+    end
+    resource do
+      Resource.first || create(:resource)
     end
   end
 end
