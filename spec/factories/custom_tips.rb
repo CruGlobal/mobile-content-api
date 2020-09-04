f5c26fdec786f16338ea1e4c0fa3036f2c9a58a7
Spencer Oberstadt <soberstadt@gmail.com>
2020-09-04 12:16:48 -0400

    fix code style

--- spec/factories/custom_tips.rb
+++ spec/factories/custom_tips.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
   factory :custom_tip do
     structure do
       <<~XML
-      <tip xmlns=\"https://mobile-content-api.cru.org/xmlns/training\" xmlns:content=\"https://mobile-content-api.cru.org/xmlns/content\" type=\"prepare\">
-        <pages>
-          <page>
-            <content:paragraph>
-              <content:text />
-              <content:text />
-            </content:paragraph>
-          </page>
-        </pages>
-      </tip>
+        <tip xmlns=\"https://mobile-content-api.cru.org/xmlns/training\" xmlns:content=\"https://mobile-content-api.cru.org/xmlns/content\" type=\"prepare\">
+          <pages>
+            <page>
+              <content:paragraph>
+                <content:text />
+                <content:text />
+              </content:paragraph>
+            </page>
+          </pages>
+        </tip>
       XML
     end
     association :tip
--- spec/factories/tips.rb
+++ spec/factories/tips.rb
@@ -2,17 +2,17 @@ FactoryBot.define do
   factory :tip do
     structure do
       <<~XML
-      <tip xmlns="https://mobile-content-api.cru.org/xmlns/training"
-        xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
-          <pages>
-              <page>
-                  <content:paragraph>
-                      <content:text />
-                  </content:paragraph>
-                  <content:text />
-              </page>
-          </pages>
-      </tip>
+        <tip xmlns="https://mobile-content-api.cru.org/xmlns/training"
+          xmlns:content="https://mobile-content-api.cru.org/xmlns/content">
+            <pages>
+                <page>
+                    <content:paragraph>
+                        <content:text />
+                    </content:paragraph>
+                    <content:text />
+                </page>
+            </pages>
+        </tip>
       XML
     end
     sequence :name do |n|
