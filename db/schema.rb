d3502c47bdd17885b4b8bb1e4216a312ac4cc8dc
Spencer Oberstadt <soberstadt@gmail.com>
2020-09-08 11:51:28 -0400

    check admin field before authorizing secure endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_170210) do
+ActiveRecord::Schema.define(version: 2020_09_08_153551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -230,6 +230,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_170210) do
     t.string "sso_guid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["sso_guid"], name: "index_users_on_sso_guid", unique: true
   end
--- spec/controllers/secure_controller_spec.rb
+++ spec/controllers/secure_controller_spec.rb
@@ -5,11 +5,15 @@ require "rails_helper"
 describe SecureController do
   controller do
     def index
+      head :ok, content_type: "text/html"
     end
   end
 
   let(:not_found_token) { "3240fa3f-d242-49e3-a03f-4d4ce68ea13d" }
   let(:expired_token) { AuthToken.encode(exp: 1.hour.ago.to_i) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:generic_token) { AuthToken.generic_token }
+  let(:user_token) { AuthToken.new(user: user).token }
 
   it "unauthorized if token is nil" do
     request.headers["Authorization"] = nil
@@ -29,6 +33,32 @@ describe SecureController do
 
   it "unauthorized if token is expired" do
     request.headers["Authorization"] = expired_token
+
+    get :index
+
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  it "successes if token is generic" do
+    request.headers["Authorization"] = generic_token
+
+    get :index
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "successes if token is for admin user" do
+    request.headers["Authorization"] = user_token
+    user.update(admin: true)
+
+    get :index
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "unauthorized if token is for non-admin user" do
+    request.headers["Authorization"] = user_token
+    user.update(admin: false)
 
     get :index
 
