f293a426fcdc76f70daea1741e06be6ed9776c50
Spencer Oberstadt <soberstadt@gmail.com>
2021-01-04 14:44:48 -0500

    fix config sync

--- bin/secure-sync.sh
+++ bin/secure-sync.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-aws s3 sync s3://${AWS_S3_CONFIG_BUCKET}/credentials.json config/secure/service_account_cred.json
+aws s3 cp s3://${AWS_S3_CONFIG_BUCKET}/credentials.json config/secure/service_account_cred.json
