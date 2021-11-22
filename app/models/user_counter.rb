c0a916349591a4b2a6e7b38a5f97720e7a41cd14
Andrew Roth <andrewroth@gmail.com>
2021-11-22 14:27:29 -0500

    standardrb fixes

--- app/models/user_counter.rb
+++ app/models/user_counter.rb
@@ -1,7 +1,7 @@
 class UserCounter < ApplicationRecord
   # This decay rate yields a half life of about 90 days with the following equation:
   # f(x, t): x * e ^ (r * t)
-  DECAY_RATE = -0.0077;
+  DECAY_RATE = -0.0077
 
   belongs_to :user
 
@@ -10,7 +10,7 @@ class UserCounter < ApplicationRecord
     days_to_decay = date_now - last_decay
 
     if days_to_decay > 0
-      self.decayed_count *= (Math::E ** (DECAY_RATE * days_to_decay))
+      self.decayed_count *= (Math::E**(DECAY_RATE * days_to_decay))
       self.last_decay = date_now
     end
   end
--- spec/acceptance/user_counters_controller_spec.rb
+++ spec/acceptance/user_counters_controller_spec.rb
@@ -60,7 +60,7 @@ resource "UserCounters" do
 
       expect(status).to eq(200)
       today = Date.today.to_s
-      expected_result = %|{"data":[{"id":"tool_opens.kgp","type":"user-counter","attributes":{"count":50,"decayed-count":25.00367978478838,"last-decay":"#{today}"}},{"id":"other.kgp","type":"user-counter","attributes":{"count":60,"decayed-count":20.002943827830705,"last-decay":"#{today}"}}]}|
+      expected_result = %({"data":[{"id":"tool_opens.kgp","type":"user-counter","attributes":{"count":50,"decayed-count":25.00367978478838,"last-decay":"#{today}"}},{"id":"other.kgp","type":"user-counter","attributes":{"count":60,"decayed-count":20.002943827830705,"last-decay":"#{today}"}}]})
       expect(response_body).to eq(expected_result)
     end
   end
