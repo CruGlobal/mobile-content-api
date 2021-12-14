fba04d84bf448ade390febf92cd32c9bbbf85f4d
Andrew Roth <andrewroth@gmail.com>
2021-12-14 13:06:46 -0500

    switch to integer foreign keys

--- db/migrate/20211201161437_create_user_counter_values.rb
+++ db/migrate/20211201161437_create_user_counter_values.rb
@@ -1,8 +1,8 @@
 class CreateUserCounterValues < ActiveRecord::Migration[6.0]
   def change
     create_table :user_counter_values do |t|
-      t.string :user_id
-      t.string :user_counter_id
+      t.integer :user_id
+      t.integer :user_counter_id
       t.string :value
 
       t.timestamps
