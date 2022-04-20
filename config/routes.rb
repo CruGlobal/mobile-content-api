23b6575dcacb766b4b7700f12bda1ff1d54b061a
Daniel Frett <frett@users.noreply.github.com>
2022-04-20 16:38:08 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Rails.application.routes.draw do
   put "resources/:id/onesky", to: "resources#push_to_onesky"
 
   get "/translations/files/:path",
-    to: redirect("https://#{ENV.fetch("MOBILE_CONTENT_API_BUCKET")}.s3.#{ENV.fetch("AWS_REGION")}.amazonaws.com#{Package::TRANSLATION_FILES_PATH}%{path}", status: 302),
+    to: redirect("https://#{ENV.fetch("MOBILE_CONTENT_API_BUCKET")}.s3.#{ENV.fetch("AWS_REGION")}.amazonaws.com/#{Package::TRANSLATION_FILES_PATH}%{path}", status: 302),
     format: false, # these next lines are required to have the extension be part of path
     default: {format: "html"},
     constraints: {path: /.*/}
