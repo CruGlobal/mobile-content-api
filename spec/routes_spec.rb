8d71779881da4be229a6c8ccdcaa3d082e93fb94
Andrew Roth <andrewroth@gmail.com>
2022-05-04 17:58:21 -0400

    use Package::TRANSLATION_FILES_PATH when possible

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 describe "routes", type: :request do
   it "redirects to s3" do
-    get "/translations/files/test.zip"
-    expect(response).to redirect_to("https://mybucket.s3.us-east-1.amazonaws.com/translations/files/test.zip")
+    get "/#{Package::TRANSLATION_FILES_PATH}test.zip"
+    expect(response).to redirect_to("https://mybucket.s3.us-east-1.amazonaws.com/#{Package::TRANSLATION_FILES_PATH}test.zip")
   end
 
   context("file without extension") do
     it "redirects to s3" do
-      get "/translations/files/test"
-      expect(response).to redirect_to("https://mybucket.s3.us-east-1.amazonaws.com/translations/files/test")
+      get "/#{Package::TRANSLATION_FILES_PATH}test"
+      expect(response).to redirect_to("https://mybucket.s3.us-east-1.amazonaws.com/#{Package::TRANSLATION_FILES_PATH}test")
     end
   end
 end
