802f4ce9c890855edaebdb0929cba0b8d9ca7754
Andrew Roth <andrewroth@gmail.com>
2022-05-04 17:50:21 -0400

    standardrb fixes

--- db/migrate/20220504203747_add_values_to_user_counters.rb
+++ db/migrate/20220504203747_add_values_to_user_counters.rb
@@ -1,8 +1,8 @@
 class AddValuesToUserCounters < ActiveRecord::Migration[6.1]
   def change
     change_table :user_counters do |t|
-      t.string 'values', array: true, default: "{}"
+      t.string "values", array: true, default: "{}"
     end
-    add_index :user_counters, :values, using: 'gin'
+    add_index :user_counters, :values, using: "gin"
   end
 end
