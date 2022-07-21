c072b2b5deeb335a727c00b1d31dd3684ac2a08f
Daniel Frett <daniel.frett@ccci.org>
2022-07-21 13:53:45 -0400

    standardrb --fix

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -12,7 +12,7 @@ class TranslationsController < ApplicationController
     if @translation.is_published
       redirect
     else
-      raise Error::NotFoundError, "Translation with ID: #{id} not found. "\
+      raise Error::NotFoundError, "Translation with ID: #{id} not found. " \
                                   "Use drafts/{id} if you're looking for an unpublished translation."
     end
   end
