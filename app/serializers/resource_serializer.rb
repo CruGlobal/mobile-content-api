b958366c5f1545002ba793355c2cb96bb2a5b6f3
Daniel Frett <daniel.frett@ccci.org>
2022-07-26 14:56:57 -0400

    sanitize the latest-drafts-translations key so it matches the output json

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -10,7 +10,7 @@ class ResourceSerializer < ActiveModel::Serializer
 
   has_many :translations
   has_many :latest_translations, key: "latest-translations"
-  has_many :latest_drafts_translations, key: "latest_drafts_translations"
+  has_many :latest_drafts_translations, key: "latest-drafts-translations"
   has_many :pages
   has_many :tips
   has_many :attachments
