b22f094ba78a90f9843c4f9bd5f87f3fb0304fa1
Daniel Frett <daniel.frett@ccci.org>
2022-08-25 13:40:34 -0400

    restore testing of the legacy patch endpoint

--- spec/acceptance/user_counters_controller_spec.rb
+++ spec/acceptance/user_counters_controller_spec.rb
@@ -74,4 +74,50 @@ resource "UserCounters" do
       expect(response_body).to eq(expected_result)
     end
   end
+
+  patch "user/counters/:id" do
+    let(:id) { "tool_opens.kgp" }
+    let(:user) { FactoryBot.create(:user) }
+    requires_okta_login
+
+    it "create user_counter" do
+      expect {
+        do_request data: {type: "user_counter", attributes: {increment: 20}}
+      }.to change { UserCounter.count }.by(1)
+
+      expect(status).to eq(200)
+      json_response = JSON.parse(response_body)["data"]
+      expect(json_response).not_to be_nil
+      expect(json_response["id"]).to eq("tool_opens.kgp")
+      expect(json_response["attributes"]["count"]).to eq(20)
+      expect(json_response["attributes"]["decayed-count"]).to eq(20)
+      expect(json_response["attributes"]["last-decay"]).to eq(Date.today.to_s)
+      expect(UserCounter.last.counter_name).to eq("tool_opens.kgp")
+      expect(UserCounter.last.count).to eq(20)
+      expect(UserCounter.last.decayed_count).to eq(20)
+      expect(UserCounter.last.last_decay).to eq(Date.today)
+    end
+
+    context "when user_counter exists" do
+      let!(:user_counter) { FactoryBot.create(:user_counter, user: user, counter_name: "tool_opens.kgp", count: 50, decayed_count: 50, last_decay: 90.days.ago) }
+
+      it "updates the count and decay" do
+        expect {
+          do_request data: {type: "user_counter", attributes: {increment: 20}}
+        }.to_not change { user_counter.count }
+
+        expect(status).to eq(200)
+        json_response = JSON.parse(response_body)["data"]
+        expect(json_response).not_to be_nil
+        expect(json_response["id"]).to eq("tool_opens.kgp")
+        expect(json_response["attributes"]["count"]).to eq(70)
+        expect((json_response["attributes"]["decayed-count"] - 45).abs).to be <= 0.004 # look within 0.004, close enough
+        expect(json_response["attributes"]["last-decay"]).to eq(Date.today.to_s)
+        expect(UserCounter.last.count).to eq(70)
+        # get close to 45 -- original value of 50 should decay to 25 with the 90 day half-life, then +20 from the patch count incremement
+        expect((UserCounter.last.decayed_count - 45).abs).to be <= 0.004
+        expect(UserCounter.last.last_decay).to eq(Date.today)
+      end
+    end
+  end
 end
