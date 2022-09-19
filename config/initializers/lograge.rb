68932cd2b48d6316e8cd08aeeb36c626488da500
Andrew Roth <andrewroth@gmail.com>
2022-09-19 14:22:31 -0400

    fix lograge websocket issue

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     exceptions = %w[controller action format id]
     {
-      params: (event.payload[:params] || event.payload[:data]).except(*exceptions)
+      params: (event.payload[:params] || event.payload[:data] || {}).except(*exceptions)
     }
   end
   config.lograge.custom_payload do |controller|
