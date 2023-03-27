ab42ea83a641c1056d11cba909022fff9659e117
Andrew Roth <andrewroth@gmail.com>
2023-03-27 12:15:09 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_155408) do
+ActiveRecord::Schema.define(version: 2023_03_21_041243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
