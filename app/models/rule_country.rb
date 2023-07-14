0cf45c77f69387b0e1083282cc1be4ab9431f7be
Cesar Diaz <cesare.diaz@gmail.com>
2023-07-14 17:18:21 -0300

    added validations to avoid duplication of country and language rules

--- app/models/rule_country.rb
+++ app/models/rule_country.rb
@@ -1,3 +1,5 @@
 class RuleCountry < ApplicationRecord
   belongs_to :tool_group
+  
+  validates :tool_group_id, uniqueness: { scope: [:countries, :negative_rule], message: "combination already exists" }
 end
--- app/models/rule_language.rb
+++ app/models/rule_language.rb
@@ -1,3 +1,5 @@
 class RuleLanguage < ApplicationRecord
   belongs_to :tool_group
+
+  validates :tool_group_id, uniqueness: { scope: [:languages, :negative_rule], message: "combination already exists" }
 end
