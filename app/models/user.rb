505ee0e7d60aa1d2a4841ab65563370c431d6a02
Daniel Frett <daniel.frett@cru.org>
2023-08-22 17:00:57 -0600

    add some validation to user_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   def set_arbitrary_attributes!(data_attrs)
     data_attrs.each_pair do |key, value|
-      attr_name = key[/^attr-(.*)$/, 1]
+      attr_name = key[/^attr-(.*)$/, 1]&.downcase
       next unless attr_name
       attr_name.tr!("-", "_")
       attribute = user_attributes.where(key: attr_name).first_or_initialize
--- app/models/user_attribute.rb
+++ app/models/user_attribute.rb
@@ -1,3 +1,8 @@
 class UserAttribute < ApplicationRecord
   belongs_to :user
+
+  validates :key, presence: true, format: {with: /\A[[:alpha:]]+(_[[:alpha:]]+)*\z/}, uniqueness: {scope: :user}
+  validates :value, presence: true
+
+  before_validation { self.key = key&.downcase }
 end
