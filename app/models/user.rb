39ab9c151be2e3a877c2e8e67038365360799e0e
Cesar Diaz <cesare.diaz@gmail.com>
2023-08-25 12:01:40 -0300

    delete dependent user_attributes for user when it is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :favorite_tools, dependent: :destroy
   has_many :tools, through: :favorite_tools
 
-  has_many :user_attributes
+  has_many :user_attributes, dependent: :destroy
 
   validates :sso_guid, uniqueness: true, presence: true, unless: -> { facebook_user_id.present? || google_user_id.present? || apple_user_id.present? }
   validates :email, presence: true
