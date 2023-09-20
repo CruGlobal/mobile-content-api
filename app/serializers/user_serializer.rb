f71f11c0aedb76a7730b40e29187f62141fc26f7
Daniel Frett <daniel.frett@cru.org>
2023-09-20 14:10:07 -0600

    use the default serializer for favorite tools

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,7 +6,7 @@ class UserSerializer < ActiveModel::Serializer
   attribute :first_name, key: "given-name"
   attribute :last_name, key: "family-name"
 
-  has_many :tools, key: "favorite-tools", serializer: ResourceFavoritedSerializer
+  has_many :tools, key: "favorite-tools"
 
   def created_at
     object.created_at.iso8601 # without this, the default serializer datetime will add 3 ms digits which we prefer not to have
