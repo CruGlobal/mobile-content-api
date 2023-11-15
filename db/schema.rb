e1177186ea126b0dc4430529b706cecdff59b760
Andrew Roth <andrewroth@gmail.com>
2023-11-15 16:21:09 -0500

    put back timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -160,6 +160,8 @@ ActiveRecord::Schema.define(version: 2023_11_02_190816) do
     t.string "key", null: false
     t.string "value", null: false
     t.boolean "is_translatable", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["key", "resource_id", "language_id"], name: "index_language_attributes_unique", unique: true
     t.index ["resource_id"], name: "index_language_attributes_on_resource_id"
   end
