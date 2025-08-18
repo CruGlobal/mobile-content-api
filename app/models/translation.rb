96b66e820ffa9da7cdbdc4bf2020a6bd6338acfa
Andrew Roth <andrewroth@gmail.com>
2025-08-18 15:47:30 -0400

    cache the translated phrases, and cleanup some unneeded methods

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -68,23 +68,22 @@ class Translation < ActiveRecord::Base
     resource.custom_manifests.find_by(language_id: language_id)&.structure || resource.manifest
   end
 
-  def manifest_translated_phrases
-    @manifest_translated_phrases ||= download_translated_phrases
-  end
-
   def push_published_to_s3
     return if is_published
 
     if resource.uses_crowdin?
       ActiveRecord::Base.transaction do
-        phrases = manifest_translated_phrases
-        name_desc_crowdin(phrases)
-        create_translated_attributes(phrases)
+        name_desc_crowdin(download_translated_phrases)
+        create_translated_attributes(download_translated_phrases)
       end
     end
 
     Package.new(self).push_to_s3
     update(is_published: true, publishing_errors: nil)
+  end
+
+  def download_translated_phrases
+    @downloaded_translated_phrases ||= CrowdinService.download_translated_phrases(language_code: language.code, project_id: resource.crowdin_project_id)
   end
 
   private
@@ -126,10 +125,6 @@ class Translation < ActiveRecord::Base
     end
 
     translation_attributes.where.not(id: translation_attribute_ids).delete_all
-  end
-
-  def download_translated_phrases
-    CrowdinService.download_translated_phrases(language_code: language.code, project_id: resource.crowdin_project_id)
   end
 
   def set_defaults
