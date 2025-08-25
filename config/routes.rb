3686ce799d307c35c7aa5d4014da6ceafde711a2
Justin Sabelko <justin.sabelko@cru.org>
2025-08-25 14:36:45 -0500

    Fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ require "sidekiq/pro/web"
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Reveal health status on /monitors/lb that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", :as => :rails_health_check
+  get "monitors/lb", :as => :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -79,7 +79,6 @@ Rails.application.routes.draw do
     resources :training_tips, path: "training-tips", only: [:create, :update, :destroy]
   end
 
-  get "monitors/lb"
   get "monitors/commit"
 
   get "attachments/:id/download", to: "attachments#download"
