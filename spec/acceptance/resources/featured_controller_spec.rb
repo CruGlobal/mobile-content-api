abb378057ee8ccdf011fa4a6a672fd2ed9869923
Hans Gamarra <hans@deepspacerobots.com>
2025-10-15 21:57:16 -0500

    Added acceptance tests

---
+++ spec/acceptance/resources/featured_controller_spec.rb
@@ -0,0 +1,133 @@
+# frozen_string_literal: true
+
+require "acceptance_helper"
+require "sidekiq/testing"
+
+resource "Resources::Featured" do
+  include ActiveJob::TestHelper
+
+  header "Accept", "application/vnd.api+json"
+  header "Content-Type", "application/vnd.api+json"
+  let(:raw_post) { params.to_json }
+  let(:authorization) { AuthToken.generic_token }
+
+  let!(:resource) { FactoryBot.create(:resource) }
+  let!(:resource_score) { FactoryBot.create(:resource_score, resource: resource, featured: true, featured_order: 1) }
+  let!(:unfeatured_resource_score) { FactoryBot.create(:resource_score, featured: false) }
+
+  get "resources/featured" do
+    context "without filters" do
+      it "returns featured resources" do
+        do_request
+
+        expect(status).to be(200)
+        json = JSON.parse(response_body)
+        expect(json["data"].size).to eq(1)
+        expect(json["data"][0]["attributes"]["featured"]).to be true
+      end
+    end
+
+    context "with language filter" do
+      let!(:resource_score_fr) { FactoryBot.create(:resource_score, lang: "fr", featured: true) }
+
+      it "returns featured resources for specified language" do
+        do_request "filter[lang]": "fr"
+
+        expect(status).to be(200)
+        json = JSON.parse(response_body)
+        expect(json["data"].size).to eq(1)
+        expect(json["data"][0]["attributes"]["lang"]).to eq("fr")
+      end
+    end
+
+    context "with country filter" do
+      let!(:resource_score_us) { FactoryBot.create(:resource_score, country: "US", featured: true) }
+
+      it "returns featured resources for specified country" do
+        do_request "filter[country]": "US"
+
+        expect(status).to be(200)
+        json = JSON.parse(response_body)
+        expect(json["data"].size).to eq(1)
+        expect(json["data"][0]["attributes"]["country"]).to eq("US")
+      end
+    end
+
+    context "with resource_type filter" do
+      let!(:tool_resource) { FactoryBot.create(:resource, resource_type: FactoryBot.create(:resource_type, name: "tool")) }
+      let!(:tool_score) { FactoryBot.create(:resource_score, resource: tool_resource, featured: true) }
+
+      it "returns featured resources for specified resource type" do
+        do_request "filter[resource_type]": "tool"
+
+        expect(status).to be(200)
+        json = JSON.parse(response_body)
+        expect(json["data"].size).to eq(1)
+        expect(json["data"][0]["relationships"]["resource"]["data"]["id"]).to eq(tool_resource.id.to_s)
+      end
+    end
+  end
+
+  post "resources/featured" do
+    requires_authorization
+
+    let(:valid_params) do
+      {
+        data: {
+          type: "resource_score",
+          attributes: {
+            resource_id: resource.id,
+            lang: "en",
+            country: "US",
+            featured: true,
+            featured_order: 1
+          }
+        }
+      }
+    end
+
+    context "with valid parameters" do
+      it "creates a new featured resource score" do
+        do_request(valid_params)
+
+        expect(status).to be(201)
+        json = JSON.parse(response_body)
+        expect(json["data"]["attributes"]["featured"]).to be true
+        expect(json["data"]["attributes"]["featured_order"]).to eq(1)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "returns unprocessable entity" do
+        do_request(data: { type: "resource_score", attributes: { featured: true } })
+
+        expect(status).to be(422)
+        json = JSON.parse(response_body)
+        expect(json).to have_key("errors")
+      end
+    end
+  end
+
+  delete "resources/featured/:id" do
+    requires_authorization
+
+    let(:id) { resource_score.id }
+
+    it "deletes the featured resource score" do
+      do_request
+
+      expect(status).to be(200)
+      expect(ResourceScore.exists?(id)).to be false
+    end
+
+    context "with non-existent resource score" do
+      let(:id) { 999999 }
+
+      it "returns not found" do
+        do_request
+
+        expect(status).to be(404)
+      end
+    end
+  end
+end
--- spec/models/resource_score_spec.rb
+++ spec/models/resource_score_spec.rb
@@ -3,5 +3,82 @@
 require "rails_helper"
 
 RSpec.describe ResourceScore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:resource) { FactoryBot.create(:resource) }
+  subject(:resource_score) { FactoryBot.build(:resource_score, resource: resource) }
+
+  describe "associations" do
+    it { is_expected.to belong_to(:resource) }
+  end
+
+  describe "validations" do
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:country) }
+    it { is_expected.to validate_presence_of(:lang) }
+
+    context "score validation" do
+      it { is_expected.to validate_numericality_of(:score).only_integer.is_greater_than_or_equal_to(1).is_less_than_or_equal_to(20).allow_nil }
+    end
+
+    context "featured_order validation" do
+      it { is_expected.to validate_numericality_of(:featured_order).only_integer.is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
+    end
+
+    context "default_order validation" do
+      it { is_expected.to validate_numericality_of(:default_order).only_integer.is_greater_than_or_equal_to(1).allow_nil }
+    end
+
+    context "uniqueness validation" do
+      before { FactoryBot.create(:resource_score, resource: resource, country: "US", lang: "en") }
+      
+      it "validates uniqueness of resource_id scoped to country and lang" do
+        duplicate = FactoryBot.build(:resource_score, resource: resource, country: "US", lang: "en")
+        expect(duplicate).not_to be_valid
+        expect(duplicate.errors[:resource_id]).to include("should have only one score per country and language")
+      end
+    end
+
+    context "featured validation" do
+      it "requires featured_order when featured is true" do
+        resource_score.featured = true
+        resource_score.featured_order = nil
+        expect(resource_score).not_to be_valid
+        expect(resource_score.errors[:featured_order]).to include("must be present if resource is featured")
+      end
+
+      it "validates uniqueness of featured_order within country and language" do
+        existing = FactoryBot.create(:resource_score, featured: true, featured_order: 1, country: "US", lang: "en")
+        duplicate = FactoryBot.build(:resource_score, featured: true, featured_order: 1, country: "US", lang: "en")
+        expect(duplicate).not_to be_valid
+        expect(duplicate.errors[:featured_order]).to include("is already taken for this country and language")
+      end
+
+      it "allows same featured_order for different country" do
+        FactoryBot.create(:resource_score, featured: true, featured_order: 1, country: "US", lang: "en")
+        different_country = FactoryBot.build(:resource_score, featured: true, featured_order: 1, country: "CA", lang: "en")
+        expect(different_country).to be_valid
+      end
+
+      it "allows same featured_order for different language" do
+        FactoryBot.create(:resource_score, featured: true, featured_order: 1, country: "US", lang: "en")
+        different_lang = FactoryBot.build(:resource_score, featured: true, featured_order: 1, country: "US", lang: "es")
+        expect(different_lang).to be_valid
+      end
+    end
+  end
+
+  describe "callbacks" do
+    context "before_save" do
+      it "downcases country" do
+        resource_score.country = "US"
+        resource_score.save
+        expect(resource_score.country).to eq("us")
+      end
+
+      it "downcases lang" do
+        resource_score.lang = "EN"
+        resource_score.save
+        expect(resource_score.lang).to eq("en")
+      end
+    end
+  end
 end
