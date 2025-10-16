91cc1c83031a2d0e8535d285b4104480ff6a3fcf
Hans Gamarra <hans@deepspacerobots.com>
2025-10-16 12:45:41 -0500

    Finished acceptance tests

--- app/serializers/resource_score_serializer.rb
+++ app/serializers/resource_score_serializer.rb
@@ -3,7 +3,7 @@
 # Serializer for ResourceScore model
 class ResourceScoreSerializer < ActiveModel::Serializer
   type "resource-score"
-  attributes :featured, :country, :lang, :score, :user_score_average, :user_score_count, :featured_order
+  attributes :featured, :country, :lang, :score, :user_score_average, :user_score_count, :featured_order, :default_order
 
   belongs_to :resource
 end
