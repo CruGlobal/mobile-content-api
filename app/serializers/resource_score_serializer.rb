88fef822b036d59b49c49c4fce73c4f3e7c68026
Hans Gamarra <hans@deepspacerobots.com>
2025-10-28 17:02:26 -0500

    Adding created_at and updated_at attrs

--- app/serializers/resource_score_serializer.rb
+++ app/serializers/resource_score_serializer.rb
@@ -2,7 +2,7 @@
 
 class ResourceScoreSerializer < ActiveModel::Serializer
   type "resource-score"
-  attributes :featured, :country, :lang, :score, :user_score_average, :user_score_count, :featured_order
+  attributes :featured, :country, :lang, :score, :user_score_average, :user_score_count, :featured_order, :created_at, :updated_at
 
   belongs_to :resource
 end
