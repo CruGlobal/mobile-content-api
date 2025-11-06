dc1c10dfd6fd937a5b80a16de38260890000fd0b
Hans Gamarra <hans@deepspacerobots.com>
2025-11-06 08:47:35 -0500

    Fixed lint issues

--- app/controllers/resources/featured_controller.rb
+++ app/controllers/resources/featured_controller.rb
@@ -22,22 +22,22 @@ module Resources
       @resource_score.save!
       render json: @resource_score, status: :created
     rescue ActiveRecord::RecordInvalid => e
-      render json: { errors: formatted_errors('record_invalid', e) }, status: :unprocessable_entity
+      render json: {errors: formatted_errors("record_invalid", e)}, status: :unprocessable_entity
     end
 
     def destroy
       @resource_score = ResourceScore.find(params[:id])
       @resource_score.destroy!
       render json: {}, status: :ok
-    rescue StandardError
-      render json: { errors: [{ source: { pointer: '/data/attributes/id' }, detail: e.message }] },
-             status: :unprocessable_entity
+    rescue
+      render json: {errors: [{source: {pointer: "/data/attributes/id"}, detail: e.message}]},
+        status: :unprocessable_entity
     end
 
     private
 
     def all_featured_resources(lang:, country:, resource_type: nil)
-      scope = Resource.includes(:resource_scores).left_joins(:resource_scores).where(resource_scores: { featured: true })
+      scope = Resource.includes(:resource_scores).left_joins(:resource_scores).where(resource_scores: {featured: true})
 
       scope = filter_by_lang(scope, lang)
       scope = filter_by_country(scope, country)
@@ -51,19 +51,19 @@ module Resources
     def filter_by_lang(scope, lang)
       return scope unless lang.present?
 
-      scope.where('resource_scores.lang = LOWER(:lang)', lang:)
+      scope.where("resource_scores.lang = LOWER(:lang)", lang:)
     end
 
     def filter_by_country(scope, country)
       return scope unless country.present?
 
-      scope.where('resource_scores.country = LOWER(:country)', country:)
+      scope.where("resource_scores.country = LOWER(:country)", country:)
     end
 
     def filter_by_resource_type(scope, resource_type)
       return scope unless resource_type.present?
 
-      scope.joins(:resource_type).where(resource_types: { name: resource_type.downcase })
+      scope.joins(:resource_type).where(resource_types: {name: resource_type.downcase})
     end
   end
 end
