4c9c7a3799322c9a590721ac45179f579303e298
Hans Gamarra <hans@deepspacerobots.com>
2025-11-06 08:42:52 -0500

    Feedback from PR

--- app/controllers/resources/featured_controller.rb
+++ app/controllers/resources/featured_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module Resources
-  # Controller to manage featured resources
   class FeaturedController < ApplicationController
     before_action :authorize!, only: %i[create destroy]
 
@@ -16,25 +15,29 @@ module Resources
     end
 
     def create
+      create_params = params.require(:data).require(:attributes).permit(
+        :resource_id, :lang, :country, :score, :featured_order, :featured, :default_order
+      )
       @resource_score = ResourceScore.new(create_params)
       @resource_score.save!
       render json: @resource_score, status: :created
     rescue ActiveRecord::RecordInvalid => e
-      render json: {errors: formatted_errors("record_invalid", e)}, status: :unprocessable_entity
+      render json: { errors: formatted_errors('record_invalid', e) }, status: :unprocessable_entity
     end
 
     def destroy
       @resource_score = ResourceScore.find(params[:id])
       @resource_score.destroy!
       render json: {}, status: :ok
-    rescue
-      render json: {errors: [{source: {pointer: "/data/attributes/id"}, detail: e.message}]}, status: :unprocessable_entity
+    rescue StandardError
+      render json: { errors: [{ source: { pointer: '/data/attributes/id' }, detail: e.message }] },
+             status: :unprocessable_entity
     end
 
     private
 
     def all_featured_resources(lang:, country:, resource_type: nil)
-      scope = Resource.includes(:resource_scores).left_joins(:resource_scores).where(resource_scores: {featured: true})
+      scope = Resource.includes(:resource_scores).left_joins(:resource_scores).where(resource_scores: { featured: true })
 
       scope = filter_by_lang(scope, lang)
       scope = filter_by_country(scope, country)
@@ -45,28 +48,22 @@ module Resources
       resources.created_at DESC")
     end
 
-    def create_params
-      params.require(:data).require(:attributes).permit(
-        :resource_id, :lang, :country, :score, :featured_order, :featured, :default_order
-      )
-    end
-
     def filter_by_lang(scope, lang)
       return scope unless lang.present?
 
-      scope.where("resource_scores.lang = LOWER(:lang)", lang:)
+      scope.where('resource_scores.lang = LOWER(:lang)', lang:)
     end
 
     def filter_by_country(scope, country)
       return scope unless country.present?
 
-      scope.where("resource_scores.country = LOWER(:country)", country:)
+      scope.where('resource_scores.country = LOWER(:country)', country:)
     end
 
     def filter_by_resource_type(scope, resource_type)
       return scope unless resource_type.present?
 
-      scope.joins(:resource_type).where(resource_types: {name: resource_type.downcase})
+      scope.joins(:resource_type).where(resource_types: { name: resource_type.downcase })
     end
   end
 end
