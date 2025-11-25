74333d31816b86cd81d6c9c9636a05d63a210599
Hans Gamarra <hans@deepspacerobots.com>
2025-11-25 10:23:39 -0500

    Updated spec for content_status

--- spec/acceptance/content_status_controller_spec.rb
+++ spec/acceptance/content_status_controller_spec.rb
@@ -10,6 +10,20 @@ resource "ContentStatus" do
   header "Content-Type", "application/vnd.api+json"
 
   get "content_status" do
+    let(:country) { "us" }
+    let!(:resource) { Resource.first }
+    let!(:unfeatured_resource) { Resource.last }
+    let!(:language_en) { Language.find_or_create_by!(code: "en", name: "English") }
+    let!(:language_fr) { Language.find_or_create_by!(code: "fr", name: "French") }
+    let!(:resource2) { Resource.joins(:resource_type).where("resource_types.name = ? AND resources.id NOT IN (?)", resource.resource_type.name, resource.id).first }
+    let!(:resource3) { Resource.joins(:resource_type).where("resource_types.name = ? AND resources.id NOT IN (?)", resource.resource_type.name, [resource.id, resource2.id]).first }
+    let!(:resource_score) do
+      ResourceScore.create!(resource: resource, country: country, language: language_en, featured: true, featured_order: 1)
+    end
+    let!(:resource_score2) do
+      ResourceScore.create!(resource: resource2, country: country, language: language_en, featured: false, featured_order: nil)
+    end
+
     it "returns statistics JSON" do
       do_request
 
