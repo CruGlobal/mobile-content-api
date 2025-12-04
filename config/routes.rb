c0ab60766ee928184761a367a950d88a93070d80
Hans Gamarra <hans@deepspacerobots.com>
2025-12-04 12:07:26 -0500

    Fixed lint issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'sidekiq/pro/web'
+require "sidekiq/pro/web"
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /monitors/lb that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'monitors/lb', as: :rails_health_check
+  get "monitors/lb", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -15,12 +15,12 @@ Rails.application.routes.draw do
   resources :systems, only: %i[index show]
   resources :languages
   resources :resource_types, only: %i[index show]
-  get 'resources/suggestions', to: 'resources#suggestions'
+  get "resources/suggestions", to: "resources#suggestions"
 
   resources :resources do
     resources :languages, controller: :resource_languages, only: %i[update show]
-    resources :translated_attributes, path: 'translated-attributes', only: %i[create update destroy]
-    post 'translations/publish', to: 'resources#publish_translation'
+    resources :translated_attributes, path: "translated-attributes", only: %i[create update destroy]
+    post "translations/publish", to: "resources#publish_translation"
     collection do
       get :featured
       resources :featured, only: %i[index create update destroy], module: :resources do
@@ -68,58 +68,58 @@ Rails.application.routes.draw do
 
   resources :custom_manifests, only: %i[create update destroy show]
 
-  resources :tool_groups, path: 'tool-groups', only: %i[create destroy index show update] do
-    post 'tools', to: 'tool_groups#create_tool'
-    put 'tools/:id', to: 'tool_groups#update_tool'
-    delete 'tools/:id', to: 'tool_groups#delete_tool'
+  resources :tool_groups, path: "tool-groups", only: %i[create destroy index show update] do
+    post "tools", to: "tool_groups#create_tool"
+    put "tools/:id", to: "tool_groups#update_tool"
+    delete "tools/:id", to: "tool_groups#delete_tool"
   end
 
   # Rule Languages
-  resources :tool_groups, path: 'tool-groups', only: [] do
-    resources :rule_languages, path: 'rules-language', only: %i[create destroy update]
+  resources :tool_groups, path: "tool-groups", only: [] do
+    resources :rule_languages, path: "rules-language", only: %i[create destroy update]
   end
 
   # Rule Countries
-  resources :tool_groups, path: 'tool-groups', only: [] do
-    resources :rule_countries, path: 'rules-country', only: %i[create destroy update]
+  resources :tool_groups, path: "tool-groups", only: [] do
+    resources :rule_countries, path: "rules-country", only: %i[create destroy update]
   end
 
   # Rule Praxis
-  resources :tool_groups, path: 'tool-groups', only: [] do
-    resources :rule_praxes, path: 'rules-praxis', only: %i[create destroy update]
+  resources :tool_groups, path: "tool-groups", only: [] do
+    resources :rule_praxes, path: "rules-praxis", only: %i[create destroy update]
   end
 
-  patch 'user/counters/:id', to: 'user_counters#update' # Legacy route for GodTools Android v5.7.0-v6.0.0
-  patch 'user/me/counters/:id', to: 'user_counters#update' # Legacy route for GodTools Android v6.0.1+
-  get 'users/:user_id/counters', to: 'user_counters#index'
-  patch 'users/:user_id/counters/:id', to: 'user_counters#update'
-  get 'users/:id', to: 'users#show'
-  delete 'users/:id', to: 'users#destroy'
-  patch 'users/:id', to: 'users#update'
+  patch "user/counters/:id", to: "user_counters#update" # Legacy route for GodTools Android v5.7.0-v6.0.0
+  patch "user/me/counters/:id", to: "user_counters#update" # Legacy route for GodTools Android v6.0.1+
+  get "users/:user_id/counters", to: "user_counters#index"
+  patch "users/:user_id/counters/:id", to: "user_counters#update"
+  get "users/:id", to: "users#show"
+  delete "users/:id", to: "users#destroy"
+  patch "users/:id", to: "users#update"
 
-  scope 'users/:user_id/relationships' do
-    resources :favorite_tools, path: 'favorite-tools', only: %i[index create]
+  scope "users/:user_id/relationships" do
+    resources :favorite_tools, path: "favorite-tools", only: %i[index create]
   end
-  delete 'users/:user_id/relationships/favorite-tools', to: 'favorite_tools#destroy'
+  delete "users/:user_id/relationships/favorite-tools", to: "favorite_tools#destroy"
 
-  scope 'users/:user_id' do
-    resources :training_tips, path: 'training-tips', only: %i[create update destroy]
+  scope "users/:user_id" do
+    resources :training_tips, path: "training-tips", only: %i[create update destroy]
   end
 
-  get 'monitors/commit'
+  get "monitors/commit"
 
-  get 'attachments/:id/download', to: 'attachments#download'
-  get 'analytics/global', to: 'global_activity_analytics#show'
+  get "attachments/:id/download", to: "attachments#download"
+  get "analytics/global", to: "global_activity_analytics#show"
 
-  get 'translations/files/:path',
-      to: redirect(
-        "https://#{ENV.fetch('MOBILE_CONTENT_API_BUCKET')}.s3.#{ENV.fetch('AWS_REGION')}.amazonaws.com/#{Package::TRANSLATION_FILES_PATH}%{path}", status: 302
-      ),
-      format: false, # these next lines are required to have the extension be part of path
-      default: { format: 'html' },
-      constraints: { path: /.*/ }
+  get "translations/files/:path",
+    to: redirect(
+      "https://#{ENV.fetch("MOBILE_CONTENT_API_BUCKET")}.s3.#{ENV.fetch("AWS_REGION")}.amazonaws.com/#{Package::TRANSLATION_FILES_PATH}%{path}", status: 302
+    ),
+    format: false, # these next lines are required to have the extension be part of path
+    default: {format: "html"},
+    constraints: {path: /.*/}
 
-  scope 'account' do
+  scope "account" do
     resources :deletion_requests, only: [:show] do
       collection do
         post :facebook
@@ -127,15 +127,15 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'content_status', to: 'content_status#index'
+  get "content_status", to: "content_status#index"
 
   if Rails.env.production? || Rails.env.staging?
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      username == ENV.fetch('SIDEKIQ_USERNAME') && password == ENV.fetch('SIDEKIQ_PASSWORD')
+      username == ENV.fetch("SIDEKIQ_USERNAME") && password == ENV.fetch("SIDEKIQ_PASSWORD")
     end
   end
-  mount Sidekiq::Web, at: '/sidekiq'
+  mount Sidekiq::Web, at: "/sidekiq"
 
-  mount ActionCable.server => '/cable'
-  mount Raddocs::App => '/docs'
+  mount ActionCable.server => "/cable"
+  mount Raddocs::App => "/docs"
 end
